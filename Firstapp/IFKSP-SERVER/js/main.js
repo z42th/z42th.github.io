522e03a38ffc0d081eb63d32ecf1fafc78698ad0
z42th <z42thq@gmail.com>
2021-09-08 00:57:15 +0200

    buttons to testtable from server data

--- Firstapp/IFKSP-SERVER/js/main.js
+++ Firstapp/IFKSP-SERVER/js/main.js
@@ -12,6 +12,50 @@ function getServerData(url) {
     );
 }
 
-getServerData("http://localhost:3000/users").then(
-    data => console.log(data)
-);
+document.querySelector("#getDataBtn").addEventListener("click",function() {
+    getServerData("http://localhost:3000/users").then(
+        data => fillDataTable(data, "userTable")
+    );
+});
+
+// Fill table with server data.
+function fillDataTable(data, tableID) {
+    let table = document.querySelector(`#${tableID}`);
+    if(!table) {
+        console.error(`Table "${tableID}" is not found.`)
+        return;
+    }
+    let tBody = table.querySelector("tbody");
+    for (let row of data) {
+        let tr = createAnyElement("tr");
+        for (let k in row) {
+            let td = createAnyElement("td");
+            td.innerHTML = row[k];
+            tr.appendChild(td);
+        }
+        let btnGroup = createBtnGroup();
+        tr.appendChild(btnGroup);
+        tBody.appendChild(tr);
+    }
+}
+
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name);
+    for(let k in attributes){
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
+
+function createBtnGroup() {
+    let group = createAnyElement("div", {class: "btn btn-group"});
+    let infoBtn = createAnyElement("button", {class: "btn btn-info"});
+    let delBtn = createAnyElement("button", {class: "btn btn-danger"});
+    
+    group.appendChild(infoBtn);
+    group.appendChild(delBtn);
+
+    let td = createAnyElement("td");
+    td.appendChild(group);
+    return td;
+}
