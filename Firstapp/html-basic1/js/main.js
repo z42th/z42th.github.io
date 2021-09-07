12e6d86400f756f05dc716aa2666efcd23f6c04d
z42th <z42thq@gmail.com>
2021-09-07 20:17:21 +0200

    event handlers

--- Firstapp/html-basic1/js/main.js
+++ Firstapp/html-basic1/js/main.js
@@ -5,3 +5,31 @@ function calcAmount(){
     alert("Fizetenőd: " + amount + "Ft");
 
 }
+
+//      Parent elem kezelése
+let alertCloseButtons = document.querySelectorAll(".close[data-dissmiss='alert']");
+let alertCloseEventHandlerFunction = function(ev){
+    this.parentElement.style.display = "none";
+};
+for (let i = 0; i < alertCloseButtons.length; i++) {
+    alertCloseButtons[i].addEventListener("click",alertCloseEventHandlerFunction);
+}
+
+//Select elem kitöltése.
+let toppings = [
+    "Szalonna",
+    "Hagyma",
+    "Tükörtojás",
+    "Libamáj",
+    "Extra sonka"
+];
+let toppingSelect = document.querySelector("#topInput");
+let index = 0;
+while (index < toppings.length) {
+    let option = document.createElement("option");
+    option.value = toppings[index];
+    option.innerHTML = toppings[index];
+    toppingSelect.appendChild(option);
+    index++;
+    
+}
